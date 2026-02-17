904e9b38c4dc5121c4564e7d6db42cf56e1c5ef1
Jesus Lopez <jeslgtz785@gmail.com>
2026-02-17 17:54:14 -0600

    query params

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI(title="mini Blog")
 
-@app.get("/")
+BLOG_POST = [
+    {"id": 1, "title": "nala", "content": "Mi primer post con nala"},
+    {"id": 2, "title": "nala", "content": "Mi primer post con nala"},
+    {"id": 3, "title": "nala", "content": "Mi primer post con nala"}
 
+]
+
+
+@app.get("/")
 def home():
-    return {'message':"bienvenidos a mini blog"}
+    return {'message': "bienvenidos a mini blog"}
+
+# data estatica a un ednpoint
+
+
+@app.get("/posts")
+def list_posts(query: str | None = Query(default=None, description="Text para buscar")):
+
+    if query:
+        results = [post for post in BLOG_POST if query.lower() in post['title'].lower()]
+        return {"data": results, "query": query}
+        # for post in BLOG_POST:
+        #     if query.lower() in post['title'].lower():
+        #         results.append(post)
+
+    return {"data": BLOG_POST}
+
