3ee4bb93ef2054025eb1205d6efe8e71d81d9bab
jesus <jesus@gmail.com>
2026-04-19 11:47:26 -0600

    nnuevos metodos optimizados pero con base de datos

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from fastapi import FastAPI, Query, HTTPException, Path, status, Depends
 from math import ceil
 from pydantic import BaseModel, Field, field_validator, EmailStr, ConfigDict
-from sqlalchemy import create_engine, Integer, String, Text, DateTime
+from sqlalchemy import create_engine, Integer, String, Text, DateTime, func, select
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase, mapped_column, Mapped
 from typing import Optional, List, Union, Literal
@@ -47,7 +47,7 @@ class PostORM():
     __tablename__ = "posts"
     # declarar columnas
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    titulo: Mapped[str] = mapped_column(
+    title: Mapped[str] = mapped_column(
         String(100), nullable=False, index=True)
     content: Mapped[str] = mapped_column(Text, nullable=False)
     create_at: Mapped[datetime] = mapped_column(
@@ -58,12 +58,12 @@ class PostORM():
 Base.metadata.create_all(bind=engine)
 
 
-app = FastAPI(titulo="mini Blog")
+app = FastAPI(title="mini Blog")
 
 BLOG_POST = [
-    {"id": 1, "titulo": "nala", "content": "Mi fiel nala"},
-    {"id": 2, "titulo": "Odie", "content": "El mas bonito"},
-    {"id": 3, "titulo": "peluche", "content": "Mi primer perro"}
+    {"id": 1, "title": "nala", "content": "Mi fiel nala"},
+    {"id": 2, "title": "Odie", "content": "El mas bonito"},
+    {"id": 3, "title": "peluche", "content": "Mi primer perro"}
 
 ]
 
@@ -79,7 +79,7 @@ class Author(BaseModel):
 
 
 class PostBase(BaseModel):
-    titulo: str
+    title: str
     content: str
     # content: Optional[str] = "Esperando contenido descriptivo"
     tags: Optional[List[Tag]] = Field(default_factory=list)
@@ -89,7 +89,7 @@ class PostBase(BaseModel):
 
 
 class PostCreate(BaseModel):
-    titulo: str = Field(
+    title: str = Field(
         ...,
         min_length=3,
         max_length=100,
@@ -108,17 +108,17 @@ class PostCreate(BaseModel):
 
     # * validacion perzonalizada -> atravez de metodo y se puede reutilizar
 
-    @field_validator("titulo")
+    @field_validator("title")
     @classmethod
     def validate_titulo_content(cls, value: str) -> str:
         if "spam" in value.lower():
-            raise ValueError("El titulo no pude contener la palabra: 'spam'")
+            raise ValueError("El title no pude contener la palabra: 'spam'")
         return value
 
 
 # * validaciones sencillas y optionales
 class postUpdate(BaseModel):
-    titulo: Optional[str] = Field(None, min_length=3, max_length=10)
+    title: Optional[str] = Field(None, min_length=3, max_length=10)
     content: Optional[str] = None
     """
     o agregarle un valor por defecto   content: Optional[str] = "valor por defecto"
@@ -132,7 +132,9 @@ class postPublic(PostBase):
 
 class Postsummary(BaseModel):
     id: int
-    titulo: str
+    title: str
+    # con esto validamos objetos y sin este es solo diccionarios
+    model_config = ConfigDict(from_attributes=True)
 
 
 class PaginatedPost(BaseModel):
@@ -156,11 +158,11 @@ def home():
 
 
 """
-los query parametres sirven para filtrar y buscar y perzonalizar una peticion 
+los query parametres sirven para filtrar y buscar y perzonalizar una peticion
   ordenado limitado etc.
-  
+
   como quiero traer ese recurso
-   
+
 """
 
 """
@@ -168,7 +170,7 @@ rewsponse model  se encragzara de hacer que compla con un molde de acuerdo a l,o
 """
 
 # validacion con QueryParameters
-""" 
+"""
 el deprecated que se usa para  cuando vamos a modificar una api y dicho paametro ya no vamos a ocupar
 """
 
@@ -202,38 +204,45 @@ def list_posts(
     ),
     direction: Literal["asc", "desc"] = Query(
         "asc", description="Direccion de orden "
-    )
+    ),
+    db: Session = Depends(get_db)
 ):
-    results = BLOG_POST
+    results = select(PostORM)
     total_pages = ceil(total/per_page) if total > 0 else 0
 
-    if total_pages == 0:
-        current_page = 1
-    else:
-        current_page = min(page, total_pages)
-
     if query:
-        results = [post for post in results if query.lower()
-                   in post['titulo'].lower()]
+        results = results.where(PostORM.title.ilike(f"%{query}%"))
         # return {"payload": results, "query": query}
 
-        total = len(results)
+        total = db.scalar(select(func.count()).select_from(
+            results.subquery())) or 0
 
-        results = sorted(
-            results, key=lambda post: post[order_by], reverse=(direction == "desc"))
+        current_page = 1 if total_pages == 0 else min(page, total_pages)
+
+        if order_by == "id":
+            order_col = PostORM.id
+        else:
+            order_col = func.lower(PostORM.title)
+
+        results = results, order_by(
+            order_col.asc() if direction == "asc" else order_col.desc())
+
+        # results = sorted(
+        #     results, key=lambda post: post[order_by], reverse=(direction == "desc"))
 
     if total_pages == 0:
-        items = []
+        items = list[PostORM] = []
     else:
         start = (current_page - 1) * per_page
-        items = results[start:start + per_page]
+        items = db.execute(results.limit(
+            per_page).offset(start)).scalars().all()
 
     has_prev = current_page > 1
     has_next = current_page < total_pages
 
     # items = results[page:offset + limit]
     # for post in BLOG_POST:
-    #     if query.lower() in post['titulo'].lower():
+    #     if query.lower() in post['title'].lower():
     #         results.append(post)
 
     # return {"payload": BLOG_POST}
@@ -267,15 +276,22 @@ def get_post(post_id: int = Path(
     description="Identifiador entero del post. deber ser mayr a uno",
     example=1
 
-), content: bool = Query(default=True, description="Incluir o no el contenido")):
-    for post in BLOG_POST:
-        if post['id'] == post_id:
-            if not content:
-                return {"id": post['id'], "titulo": post['titulo']}
-            return post
-    return HTTPException(
-        status_code=404, detail="Post no encontrado"
-    )
+), content: bool = Query(default=True, description="Incluir o no el contenido"), db: Session = Depends(get_db)):
+
+    post_find = select(PostORM).where(PostORM.id == post_id)
+    post = db.execute(post_find).scalar_one_or_none()
+
+#    post = db.get(PostORM, post_id)
+
+    if not post:
+        raise HTTPException(
+            status_code=404, detail="Post no encontrado"
+        )
+
+    if content:
+        return postPublic.model_validate(post, from_attributes=True)
+
+    return Postsummary.model_validate(post, from_attributes=True)
 
 
 """
@@ -286,7 +302,7 @@ Metodo Post
 
 @app.post("/posts", response_model=postPublic, response_description="metodo post (ok)", status_code=status.HTTP_201_CREATED)
 def create_posts(post: PostCreate, db: Session = Depends(get_db)):
-    new_Post = PostORM(titulo=post.titulo, content=post.content)
+    new_Post = PostORM(title=post.title, content=post.content)
 
     try:
         db.add(new_Post)
@@ -301,7 +317,7 @@ def create_posts(post: PostCreate, db: Session = Depends(get_db)):
 #     new_id = (BLOG_POST[-1]["id"]+1) if BLOG_POST else 1
 # # model dump transforma  de objeto a diccionario
 #     new_post = {"id": new_id,
-#                 "titulo": post.titulo,
+#                 "title": post.title,
 #                 "content": post.content,
 #                 "tags": [tag.model_dump() for tag in post.tags],
 #                 "author": post.author.model_dump() if post.author else None
@@ -339,29 +355,41 @@ Metodo put
 
 
 @app.put("/posts/{post_id}", response_model=postPublic)
-def update_post(post_id: int, payload: postUpdate):
-    for post in BLOG_POST:
-        if post["id"] == post_id:
-            payload = payload.model_dump(exclude_unset=True)
-            if "titulo" in payload:
-                post["titulo"] = payload["titulo"]
-            if "content" in payload:
-                post["content"] = payload["content"]
-            return post
+def update_post(post_id: int, payload: postUpdate, db: Session = Depends(get_db)):
 
-    raise HTTPException(status_code=404, detail="No se encontro el post")
+    post = db.get(PostORM, post_id)
+
+    if not post:
+        raise HTTPException(status_code=404, detail="No se encontro el post")
+
+    updates = payload.model_dump(exclude_unset=True)
+
+    for key, value in updates.items():
+        setattr(post, key, value)
+
+    db.add(post)
+    db.commit()
+    db.refresh(post)
+
+    return post
 
 
 """
-metodo delete 
+metodo delete
 """
 
 
-@app.delete("/posts/{post_id}", status_code=204)
-def deletePost(post_id: int):
-    for index, post in enumerate(BLOG_POST):
-        BLOG_POST.pop(index)
-        return
-    raise HTTPException(status_code=404, detail="Post no encontrado")
+@app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+def deletePost(post_id: int, db: Session = Depends(get_db)):
+
+    post = db.get(PostORM, post_id)
+
+    if not post:
+        raise HTTPException(status_code=404, detail="No se encontro el post")
+
+    db.delete(post)
+    db.commit()
+    
+    return 
 
 # *
