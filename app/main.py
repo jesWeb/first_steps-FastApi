e22acf47d0cdb8f313d44035ff0e7a709632d698
jesus <jesus@gmail.com>
2026-05-12 23:32:30 -0600

    modularizacion: add feature first structure pn db

--- app/main.py
+++ app/main.py
@@ -10,33 +10,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./blog.db")
-#print("conectado a :", DATABASE_URL)
-
-engine_kwargs = {}
-
-if DATABASE_URL.startswith("sqlite"):
-    engine_kwargs['connect_args'] = {"check_same_thread": False}
-
-
-engine = create_engine(DATABASE_URL, echo=True, future=True)
-
-SessionLocal = sessionmaker(
-    bind=engine, autoflush=False, autocommit=False, class_=Session
-)
-
-
-class Base(DeclarativeBase):
-    pass
-
-
-def get_db():
-    db = SessionLocal()
-
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 """
