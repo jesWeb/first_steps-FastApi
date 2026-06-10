ec7b7303ce7051c71e47b0951df5be09583a38c1
Jesus Lopez <jeslgtz785@gmail.com>
2026-06-10 17:14:06 -0600

    servir archivos staticos

--- app/main.py
+++ app/main.py
@@ -1,20 +1,29 @@
+import os
 from fastapi import FastAPI
 from app.core.db import Base, engine
 from dotenv import load_dotenv
 from app.api.v1.posts.router import router as post_router
 from app.api.v1.auth.router import router as auth_router
 from app.api.v1.uploads.router import router as upload_router
-
+from fastapi.staticfiles import StaticFiles
 load_dotenv()
 # solo en desarollo ya en porduccion usa migraciones
 
-def create_app()->FastAPI:
+MEDIA_DIR = "app/media"
+
+
+def create_app() -> FastAPI:
     app = FastAPI(title="mini Blog")
     Base.metadata.create_all(bind=engine)
-    app.include_router(auth_router,prefix="/api/v1")
+    app.include_router(auth_router, prefix="/api/v1")
     app.include_router(post_router)
     app.include_router(upload_router)
+
+    #asegurar que la carpeta de cargas exista o bien la ve crear en casso de que mno 
+    os.makedirs(MEDIA_DIR, exist_ok=True)
+
     return app
+
 
 app = create_app()
 
