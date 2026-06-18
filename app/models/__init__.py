b803242a9719695485882a2d1a5492add92df28e
Jesus Lopez <jeslgtz785@gmail.com>
2026-06-18 09:43:47 -0600

    modificacion en como llamamos los archivos

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 """FORMA DE IMPORTAR los archivos o completos"""
 from .author import AuthorORM
+from .tag import TagORM
 from .post import PostORM, post_tags
-from .tag import TagOrm
 
-__all__ = ["AuthorORM", "TagOrm", "PostORM", "post_tags"]
+__all__ = ["AuthorORM", "TagORM", "PostORM", "post_tags"]
